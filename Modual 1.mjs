62da7c43e409055921189c9d82cc25c2960af295
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-04 18:42:50 +0530

    Add mjs file

---
+++ Modual 1.mjs
@@ -0,0 +1,7 @@
+import abc , {b,c,d,e} from './Modual 2.mjs';
+
+console.log(abc);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
---
+++ Modual 2.mjs
@@ -0,0 +1,12 @@
+const a = "DELL";
+const b = "HP";
+const c = "ASUS";
+const d = "LENOVO";
+const e = "JIO";
+
+
+export default a;
+export {b};
+export {c};
+export {d};
+export {e};
