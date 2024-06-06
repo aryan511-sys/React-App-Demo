ec2508ba476af9b546cbf2a7c9fc90e9bec69bb7
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-06 18:00:50 +0530

    Final Converter

--- src/Cromponents/TextForm.js
+++ src/Cromponents/TextForm.js
@@ -10,16 +10,51 @@ export default function TextForm(props) {
 
     const handleUpChange = () => {
         // console.log("Btn Click");
-        let newText =text.toUpperCase();
+        let newText = text.toUpperCase()
         setText(newText);
     }
 
+    const handleWithChange = () => {
+        // console.log("Btn Click");
+        let newText = text.toLowerCase()
+        setText(newText);
+    }
+
+
+    const handleSpeak = () => {
+        let newText = new SpeechSynthesisUtterance();
+        newText.text = text;
+        window.speechSynthesis.speak(newText);
+    } 
+    
+    const clearText = () => {
+        // console.log("Btn Click");
+        let newText = "";
+        setText(newText);
+    }
+
+
+    
+ 
     return (
         <div>
-            <div className="m-5">
+            <div className="m-5 container">
                 <h1 className="">{props.heading}</h1>
                 <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" value={text} onChange={handleOnChange}></textarea>
-                <button className='btn btn-outline-danger mt-3' onClick={handleUpChange}>Convert To Upper Case</button>
+                <button className='btn btn-outline-danger mt-3' onClick={handleUpChange}>Convert To Upper Case</button>&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-outline-dark mt-3' onClick={handleWithChange}>Convert To Lower Case</button>&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-outline-success mt-3' onClick={handleSpeak}>Speak</button>&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-outline-primary mt-3' onClick={clearText} id='clear'>Clear Text</button>
+                
+            </div>
+
+            <div className="container">
+                <h2>Your Text Summary</h2>
+                <p>{text.split(" ").length}Words</p>
+                <p>{text.length}Characters</p>
+                <p>{0.008 * text.split("").length}Reading Time</p>
+                <h3>Previews</h3>
+                <p>{text}</p>
             </div>
         </div>
     )
