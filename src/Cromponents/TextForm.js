29d485aa0a7d2393032baaeb283b3c88f208d60a
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-06 16:44:29 +0530

    Add some changes

--- src/Cromponents/TextForm.js
+++ src/Cromponents/TextForm.js
@@ -1,12 +1,25 @@
 import React,{useState} from 'react'
 export default function TextForm(props) {
     var [text , setText] = useState("Enter your Text")
+
+    const handleOnChange = (event) => {
+        // console.log("On Change");
+        setText(event.target.value);
+    
+    }
+
+    const handleUpChange = () => {
+        // console.log("Btn Click");
+        let newText =text.toUpperCase();
+        setText(newText);
+    }
+
     return (
         <div>
             <div className="m-5">
                 <h1 className="">{props.heading}</h1>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" value={text}></textarea>
-                <button className='btn btn-outline-success mt-3'>Convert To Upper Case</button>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" value={text} onChange={handleOnChange}></textarea>
+                <button className='btn btn-outline-danger mt-3' onClick={handleUpChange}>Convert To Upper Case</button>
             </div>
         </div>
     )
