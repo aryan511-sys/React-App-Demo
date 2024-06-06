611716077eed7cfc5e1f8baa18f4b51994adb1dc
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-06 18:03:08 +0530

    clear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './Cromponents/Navbar.js'
-import TextForm from './Cromponents/TextForm.js'
+
 
 function App() {
   return (
   <>
-  <Navbar title = "SIT" aboutText="Abour Us"/>
-  <TextForm heading="Enter your text"/>
+  
   </>
   );
 }
