2ca306a2a0a9bced5e945cd2ee6d9a202ed070d0
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-08 14:50:58 +0530

    Add aboutus file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './Cromponents/Navbar.js'
-import TextForm from './Cromponents/TextForm.js'
+// import TextForm from './Cromponents/TextForm.js'
+import AboutUs from './Cromponents/AboutUs';
 
 function App() {
   return (
   <>
   <Navbar title = "SIT" aboutText="Abour Us"/>
-  <TextForm heading="Enter your text"/>
-  </>
+  {/* <TextForm heading="Enter your text"/> */}
+  <AboutUs title="About Us" />
+  </> 
   );
 }
 
