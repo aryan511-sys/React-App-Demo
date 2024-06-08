f6464097a8ca343f6c278ed4d1751dbc271541b2
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-08 17:14:13 +0530

    Ad Light and Dark Toggle Mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './Cromponents/Navbar.js'
 import TextForm from './Cromponents/TextForm.js'
 
 function App() {
+
+  const [mode , setMode] = useState('light')
+
+  const toogleMode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      document.body.style.background = "black"
+      document.body.style.transition = ".5s"
+      document.body.style.color = "white"
+    } else {
+      setMode('light')
+      document.body.style.background = "white"
+      document.body.style.transition = ".5s"
+      document.body.style.color = "black"
+    }
+  }
+
   return (
   <>
-  <Navbar title = "SIT" aboutText="Abour Us"/>
+  <Navbar title = "SIT" aboutText="Abour Us" mode={mode} toogleMode={toogleMode}/>
   <TextForm heading="Enter your text"/>
   </>
   );
