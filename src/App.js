e4e3f6ed616fb3916798655383018896cf43c8e5
aryan511-sys <gajerarajesh4317@gmail.com>
2024-06-19 17:54:52 +0530

    Add Some Changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './Cromponents/Navbar.js'
 import TextForm from './Cromponents/TextForm.js'
+import Alert from './Cromponents/Alert.js';
 
 function App() {
 
   const [mode , setMode] = useState('light')
+
+  const [alert , setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() =>{
+      setAlert(null);
+    } , 1500)
+  }
 
   const toogleMode = () => {
     if (mode === 'light') {
@@ -13,11 +26,19 @@ function App() {
       document.body.style.background = "black"
       document.body.style.color = "white"
       setmyText("ડાર્ક મોડ ચાલુ છે.");
+      showAlert("Dark mode has been Enable" , "success");
+      setInterval(() => {
+        document.title = "SIT - Admission"
+      }, 150)
+      setInterval(() => {
+        document.title = "SIT - B.sc.IT"
+      }, 200)
     } else {
       setMode('light')
       document.body.style.background = "white"
       document.body.style.color = "black"
       setmyText("લાઇટ મોડ ચાલુ છે.");
+      showAlert("Light mode has been Enable" , "success")
     }
   }
 
@@ -26,7 +47,9 @@ function App() {
   return (
   <>
   <Navbar title = "SIT" aboutText="Abour Us" mode={mode} toogleMode={toogleMode} myText={myText}/>
-  <TextForm heading="Enter your text" mode={mode} toogleMode={toogleMode}/>
+  <Alert alert={alert}/>
+  <TextForm heading="Enter your text" mode={mode} toogleMode={toogleMode} showAlert={showAlert}/>
+
   </>
   );
 }
